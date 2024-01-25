9609cc96ad4ccecfeec43dc2ea9c38543491c8e5
AGL_Phạm Văn Lam Sơn(ソン) <100652884+son-agl@users.noreply.github.com>
2024-01-25 19:22:01 +0700

    Update authorization.blade.php

--- src/views/authorization.blade.php
+++ src/views/authorization.blade.php
@@ -1,8 +1,10 @@
+<!doctype html>
 <html>
 
 <head>
-    <title>Welcome</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
 </head>
 
 <body>
@@ -18,48 +20,47 @@
         </div>
     @endif
 
-    <div id="authorization_page_content">
-        <form method="post" action="{{ route('authorization.update') }}">
-            @csrf
-            <button type="submit">Update</button>
-            <div class="table">
-                <table id="authorization_table" class="table table-hover table-bordered table-striped display dataTable"
-                    width="100%">
-                    <thead>
-                        <tr>
-                            <th class="text-center">STT</th>
-                            <th class="text-center">Quyền</th>
-                            <th class="text-center">Miêu tả</th>
-                            @foreach ($roles as $role)
-                                <th class="text-center">{{ $role->display_name }}</th>
-                            @endforeach
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($permissions as $permission)
-                            <tr>
-                                <td class="text-center">
-                                    {{ $loop->iteration + ($permissions->currentPage() - 1) * $permissions->perPage() }}
-                                </td>
-                                <td class="text-center">{{ $permission->display_name }}</td>
-                                <td class="text-center">{{ $permission->description }}</td>
-                                @foreach ($roles as $role)
-                                    <td class="text-center">
-                                        <input type="hidden" name="{{ "$role->id[$permission->id]" }}"
-                                            value="{{ $role->hasPermission($permission) ? 1 : 0 }}">
-                                        <input type="checkbox"
-                                            onclick="this.previousElementSibling.value=1-this.previousElementSibling.value"
-                                            {{ $role->hasPermission($permission) ? 'checked' : '' }}>
-                                    </td>
-                                @endforeach
-                            </tr>
+    <form method="post" action="{{ route('authorization.update') }}">
+        @csrf
+        <div class="text-right m-1">
+            <button type="submit" class="">Update</button>
+        </div>
+
+        <table class="table-auto">
+            <thead>
+                <tr>
+                    <th class="">STT</th>
+                    <th class="">Quyền</th>
+                    <th class="">Miêu tả</th>
+                    @foreach ($roles as $role)
+                        <th class="">{{ $role->display_name }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($permissions as $permission)
+                    <tr>
+                        <td class="">
+                            {{ $loop->iteration + ($permissions->currentPage() - 1) * $permissions->perPage() }}
+                        </td>
+                        <td class="">{{ $permission->display_name }}</td>
+                        <td class="">{!! nl2br($permission->description) !!}</td>
+                        @foreach ($roles as $role)
+                            <td class="">
+                                <input type="hidden" name="{{ "$role->id[$permission->id]" }}"
+                                    value="{{ $role->hasPermission($permission) ? 1 : 0 }}">
+                                <input type="checkbox"
+                                    onclick="this.previousElementSibling.value=1-this.previousElementSibling.value"
+                                    {{ $role->hasPermission($permission) ? 'checked' : '' }}>
+                            </td>
                         @endforeach
-                    </tbody>
-                </table>
-        </form>
-        {{ $permissions->links() }}
-    </div>
-    </div>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </form>
+
+    {{ $permissions->links() }}
 </body>
 
 </html>
